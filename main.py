6120858589241cfc8abb053dca95a31089428c4b
beelzebul-hub1 <stephendavis664+1@gmail.com>
2026-07-26 10:20:25 -0500

    Update main.py

--- main.py
+++ main.py
@@ -42,7 +42,7 @@ threading.Thread(target=run_flask, daemon=True).start()
 
 # ---------------- MINER ----------------
 twitch_miner = TwitchChannelPointsMiner(
-    username="NightFragger343",
+    username="nightfragger343",
     password="k8pSK#dB4N%1BWdw5Adt",
     claim_drops_startup=False,
     priority=[
@@ -134,7 +134,7 @@ def export_points_loop():
             # send to website
             try:
                 requests.post(
-                    "https://render-b1yk.onrender.com/api/update",
+                    "https://render-1-ethy.onrender.com/api/update",
                     json=data,
                     timeout=10
                 )
@@ -156,7 +156,7 @@ threading.Thread(target=export_points_loop, daemon=True).start()
 # ---------------- RUN -----------------
 twitch_miner.mine(
     [
-        Streamer("imshayph"),
+        Streamer("shayph"),
         Streamer("sirdapper"),
         Streamer("waffletrades_"),
     ],
